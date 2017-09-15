7430f9c066bff3f9a577270970820d73e8591c9b
Mohammad Reza Parsaeian <programmer.net2009@live.com>
2017-09-15 21:50:43 +0430

    update

--- src/Parsidev/Encryption/Encryption.php
+++ src/Parsidev/Encryption/Encryption.php
@@ -12,12 +12,43 @@ class Encryption
     {
         $this->keys = $keys;
     }
-	
-	public function encrypt($plainText){
-		
-	}
-	
-	public function decrypt($decyptedText){
-		
-	}
+
+    protected function getCode($key, $text)
+    {
+        $outText = '';
+        for ($i = 0; $i < strlen($text);) {
+            for ($j = 0; ($j < strlen($key) && $i < strlen($text)); $j++, $i++) {
+                $outText .= $text{$i} ^ $key{$j};
+            }
+        }
+        return ($outText);
+    }
+
+    public function encrypt($plainText)
+    {
+        $encrypted = $plainText;
+        foreach ($this->keys as $key) {
+            $encrypted = $this->getCode($key, $encrypted);
+        }
+        return hex2bin($encrypted);
+    }
+
+    public function decrypt($decryptedText)
+    {
+        $decrypted = $this->hex2bin($decryptedText);
+        for ($i = count($this->keys) - 1; $i >= 0; $i--) {
+            $decrypted = $this->getCode($this->keys[$i], $decrypted);
+        }
+        return $decrypted;
+    }
+
+    protected function hex2bin($hexData)
+    {
+        $binData = '';
+        for ($i = 0; $i < strlen($hexData); $i += 2) {
+            $binData .= chr(hexdec(substr($hexData, $i, 2)));
+        }
+        return $binData;
+    }
+
 }
