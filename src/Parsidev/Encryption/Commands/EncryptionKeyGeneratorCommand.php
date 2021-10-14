e3322a957791a4befbee91b24c2663b1ca2b09d2
Mohammad Reza <parsidev@users.noreply.github.com>
2021-10-14 22:47:27 +0330

    Update EncryptionKeyGeneratorCommand.php

--- src/Parsidev/Encryption/Commands/EncryptionKeyGeneratorCommand.php
+++ src/Parsidev/Encryption/Commands/EncryptionKeyGeneratorCommand.php
@@ -92,7 +92,7 @@ return [
         $outText = '';
         for ($i = 0; $i < strlen($text);) {
             for ($j = 0; ($j < strlen($key) && $i < strlen($text)); $j++, $i++) {
-                $outText .= $text{$i} ^ $key{$j};
+                $outText .= $text[$i] ^ $key[$j];
             }
         }
         return ($outText);
